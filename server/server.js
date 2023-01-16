4a029286be0b89aef9c12111f9735ca35025979d
Tiza Nege <57755279+user624-47@users.noreply.github.com>
2023-01-16 12:58:42 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,26 +15,32 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Initialize the conversation context variable
+let conversationContext = "";
+
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Ask Tiza!'
   })
 })
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-
+    // Store the previous conversation context
+    conversationContext = `${conversationContext} ${prompt}`;
+    // Use the previous conversation context in the prompt
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      prompt: conversationContext,
+      temperature: 0, 
+      max_tokens: 1000,
+      top_p: 1, 
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     });
 
+    conversationContext = `${conversationContext} ${response.data.choices[0].text}`;
     res.status(200).send({
       bot: response.data.choices[0].text
     });
